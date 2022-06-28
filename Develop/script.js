bc844924a65b38e043820962332aac167db141db
Annie Hosch <anniehosch@Annies-Air.lan>
2022-06-28 00:28:15 -0400

    set up var length and cleared up sections

--- Develop/script.js
+++ Develop/script.js
@@ -7,32 +7,43 @@ var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var element = document.getElementById("generate");
-  element.onclick = function(generatePassword) {
-  console.log(generatePassword);
-  var lowercase = prompt("Would you like lowercase charecters in your password? Type yes or no as your response.");
-  if (person == "Yes" || person == "yes") {
-    text = "User cancelled the prompt.";
-  } else {
-    text = "Hello " + person + "! How are you today?";
-  }
-  document.getElementById("generate").innerHTML = text;
+ function generatePassword() {
+    var element = document.getElementById("generate");
+    element.onclick = function(generatePassword) {
+    console.log(generatePassword);
+    parseInt(prompt("How long would you like your password length to be? Select a number from 8 to 128"))
+      if (enter < 8 || enter >128) {
+        alert("Please select a number from 8-128!")
+      else (enter = var length)
+      console.log(length)
+      }
+      document.getElementById("generate").innerHTML = text;
+    cl
+  }}
 
 
-  var parameters = getParameters ();
+//   var lowercase = prompt("Would you like lowercase charecters in your password? Type yes or no as your response.");
+//   if (person == "Yes" || person == "yes") {
+//     text = "User cancelled the prompt.";
+//   } else {
+//     text = "Hello " + person + "! How are you today?";
+//   }
+//   document.getElementById("generate").innerHTML = text;
 
-  for (i=0, i<parameters.length; i++)
 
-}}
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   var parameters = getParameters ();
 
-  passwordText.value = password;
-}
+//   for (i=0, i<parameters.length; i++)
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// }}
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
