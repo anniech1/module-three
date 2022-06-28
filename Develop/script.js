67b185a97e68d7d5231242bf1d70b4834e56d106
Annie Hosch <anniehosch@Annies-Air.lan>
2022-06-27 22:03:07 -0400

    added var for cases and numbers, made it so button when pressed has a popup message, yes or no responses

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,26 @@
 // Assignment code here
-
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "0123456789"
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  var element = document.getElementById("generate");
+  element.onclick = function(event) {
+  console.log(event);
+
+  var lowercase = prompt("Would you like lowercase charecters in your password? Type yes or no as your response.");
+  if (person == "Yes" || person == "yes") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("generate").innerHTML = text;
+}}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,11 +28,9 @@ function writePassword() {
 
   passwordText.value = password;
 
+  return password
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-//starter
