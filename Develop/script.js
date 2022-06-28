626602fafc9e6b3039b95f9ef47bbd8f964e6daa
Annie Hosch <anniehosch@Annies-Air.lan>
2022-06-28 01:34:58 -0400

    adding yes or no to prompt questions

--- Develop/script.js
+++ Develop/script.js
@@ -10,25 +10,27 @@ var generateBtn = document.querySelector("#generate");
  function generatePassword() {
     var element = document.getElementById("generate");
     element.onclick = function(generatePassword) {
-    console.log(generatePassword);
+    console.log(generatePassword);}
+
     parseInt(prompt("How long would you like your password length to be? Select a number from 8 to 128"))
       if (enter < 8 || enter >128) {
         alert("Please select a number from 8-128!")
-      else (enter = var length)
+      else (enter = var length) {
+        alert("Thank you!")
       console.log(length)
-      }
+      }}
       document.getElementById("generate").innerHTML = text;
-    cl
-  }}
+
+    var lowercase = prompt("Would you like lowercase charecters in your password? Type yes or no as your response.");
+  if (person == "Yes" || person == "yes") {
+    text = "";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("generate").innerHTML = text;
+  }
 
 
-//   var lowercase = prompt("Would you like lowercase charecters in your password? Type yes or no as your response.");
-//   if (person == "Yes" || person == "yes") {
-//     text = "User cancelled the prompt.";
-//   } else {
-//     text = "Hello " + person + "! How are you today?";
-//   }
-//   document.getElementById("generate").innerHTML = text;
 
 
 //   var parameters = getParameters ();
